5a06295ae4bfb0a421b74e92cb80c0a18dfb9d9d
tae-jun <i2r.jun@gmail.com>
2015-01-31 17:01:22 +0900

    Ignore .idea

--- src/chromium/main.js
+++ src/chromium/main.js
@@ -4,6 +4,7 @@ function run() {
     childProc.exec('chromium --kiosk --incognito --display=:0 http://ktj7147.iptime.org', function (err, stdout, stderr) {
         if (err)
             return console.log(err);
+        console.log('+++ Chromium started');
     });
 }
 exports.run = run;
--- src/chromium/main.ts
+++ src/chromium/main.ts
@@ -5,5 +5,7 @@ export function run() {
     childProc.exec('chromium --kiosk --incognito --display=:0 http://ktj7147.iptime.org', (err, stdout, stderr)=> {
         if (err)
             return console.log(err);
+
+        console.log('+++ Chromium started');
     });
 }
